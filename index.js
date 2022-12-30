7c6659d664c8888e5d43cef42b5aa1eb5feb20b6
sourav-mahmud <souravmahmud32@gmail.com>
2022-12-30 18:46:39 +0600

    post method added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,8 +9,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async(req, res) =>{
-    res.send('Task Hero Server Is Running')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhklnue.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.listen(port, () => console.log(`Hero Task server running on ${port}`))
+async function run() {
+    try{
+        const addedTaskCollections = client.db('taskHero').collection('addedTask');
+        app.post("/addtask", async(req, res) =>{
+            const task = req.body;
+            const result = await addedTaskCollections.insertOne(task);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Task Hero Server Is Running");
+});
+
+app.listen(port, () => console.log(`Hero Task server running on ${port}`));
