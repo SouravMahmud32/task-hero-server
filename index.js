3eee1ac4183b5d1d9ecc735ac1aa882f5eb80697
sourav-mahmud <souravmahmud32@gmail.com>
2022-12-30 16:17:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req, res) =>{
+    res.send('Task Hero Server Is Running')
+});
+
+app.listen(port, () => console.log(`Hero Task server running on ${port}`))
